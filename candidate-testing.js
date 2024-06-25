1a126fc4d60222f5036a9eb6d660b36f87f3ff0e
jmalone487 <jmalone487@gmail.com>
2024-06-24 19:28:03 -0500

    Commit message

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,7 +15,7 @@ let questions = [
   "True or false: 5 kilometer == 5000 meters? ",
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  "What is the minimum crew size for the International Space Station (ISS)? "
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride",
